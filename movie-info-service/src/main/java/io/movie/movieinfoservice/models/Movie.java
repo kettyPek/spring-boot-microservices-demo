1e00db61d06e40e1299fd27c7980c4db733399af
kettyPek <ketty.pekarsky@gmail.com>
2022-09-06 13:06:45 +0300

    Add Movie Info Service API

---
+++ movie-info-service/src/main/java/io/movie/movieinfoservice/models/Movie.java
@@ -0,0 +1,15 @@
+package io.movie.movieinfoservice.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Movie {
+
+	protected String movieId;
+	protected String name;
+
+}
---
+++ movie-info-service/src/main/java/io/movie/movieinfoservice/resources/MovieResource.java
@@ -0,0 +1,18 @@
+package io.movie.movieinfoservice.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.movie.movieinfoservice.models.Movie;
+
+@RestController
+@RequestMapping("/movies")
+public class MovieResource {
+	
+//	@RequestMapping("/{movieId}")
+//	public Movie getMovieInfo(@PathVariable String movieId) {
+//		
+//	}
+
+}
